52090b2aec38e9add8a5b05f600be0f73bef3c0a
Izabela Lima <izamglima@gmail.com>
2020-07-22 16:03:06 +0200

    Moved logic to charts service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { ChartsService } from './charts.service';
-import { Chart } from 'chart.js';
 
 @Component({
   selector: 'app-root',
@@ -16,121 +15,8 @@ export class AppComponent {
   constructor(private _charts: ChartsService) {}
   
   ngOnInit() {
-    let values = this._charts.randomDataToChartSpendingsHome();
-
-    this.spendingChart = new Chart('canvas', {
-      type: 'pie',
-      data: {
-        labels:  values.labelSpendings ,
-        datasets: [{
-          backgroundColor: [
-            "#F8CBC5",
-            "#D9AD5B",
-            "#5CA4EB"
-          ],
-          data: values.dataSpendings
-        }] 
-      },
-      options: {
-        responsive: true,
-        legend: {
-          position: 'bottom',
-          padding: 10
-        },
-        animation: {
-          animateScale: true,
-          animateRotate: true
-        }
-      }
-    });
-
-    this.priceChart = new Chart('canvas2', {
-      type: 'line',
-      data: {
-        labels: values.dataPriceMonths,
-        datasets: [
-          {
-            label: "Banana",
-            fontColor: "white",
-            fill: false,
-            backgroundColor: '#fff',
-            borderColor: '#fff',
-            data: [
-              this._charts.randomScalingFactor(),
-              this._charts.randomScalingFactor(),
-              this._charts.randomScalingFactor(),
-              this._charts.randomScalingFactor(),
-              this._charts.randomScalingFactor(),
-              this._charts.randomScalingFactor(),
-              this._charts.randomScalingFactor()
-            ],
-          }/*,
-          {
-          label: "Apple",
-          backgroundColor: '#45BFB3',
-          borderColor: '#45BFB3',
-          data: [
-            this._charts.randomScalingFactor(),
-            this._charts.randomScalingFactor(),
-            this._charts.randomScalingFactor(),
-            this._charts.randomScalingFactor(),
-            this._charts.randomScalingFactor(),
-            this._charts.randomScalingFactor(),
-            this._charts.randomScalingFactor()
-          ],
-          fill: false,
-        } */]
-      },
-      options: {
-        legend: {
-            label: {
-              fontColor: "white"
-            }
-        },
-        scales: { 
-          yAxes: [{
-            ticks: {
-                fontColor: "white"
-            }
-          }],
-          xAxes: [{
-            ticks: {
-              fontColor: "white"
-            }
-          }]
-        }
-      }
-    });
-
-    this.incomeChart = new Chart('canvas3', {
-      type: 'bar',
-      data: {
-        labels: values.dataIncomeMonths,
-        datasets: [{
-          label: '$',
-          data: values.dataIncomeValues,
-          backgroundColor: '#C4C4C4'
-        }]
-      },
-      options: {
-        scales: {
-          offset: false,
-          yAxes: [{
-            gridLines: {
-              drawBorder: false,
-            },
-            ticks: {
-              beginAtZero: true
-            }
-          }],
-          xAxes: {
-            gridLines: {
-              drawBorder: false,
-              offsetGridLines: false
-            }
-          }
-        }
-      }
-    });
+    this.spendingChart = this._charts.buildSpendingChart('canvas');
+    this.priceChart = this._charts.buildPriceChart('canvas2');
+    this.incomeChart = this._charts.buildIncomeChart('canvas3');
   }
 }
--- src/app/charts.service.ts
+++ src/app/charts.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs';
 import SampleJson from '../assets/randomDataToCharts.json';
+import { Chart } from 'chart.js';
 
 @Injectable({
   providedIn: 'root'
@@ -12,17 +13,139 @@ export class ChartsService {
   constructor() {
 
   }
-
+  // Exemplo pra quando integrar com o backend
   /*dailyForecast() {
     return this._http.get("http://samples.openweathermap.org/data/2.5/history/city?q=Warren,OH&appid=b6907d289e10d714a6e88b30761fae22")
       .map(result => result);
   }*/
   
-  randomDataToChartSpendingsHome() {
+  randomData() {
     return SampleJson;
   }
 
   randomScalingFactor = function() {
     return (Math.random() > 0.5 ? 1.0 : -1.0) * Math.round(Math.random() * 100);
+  }
+
+
+  buildPriceChart = function(element:string) {
+    return new Chart(element, {
+      type: 'line',
+      data: {
+        labels: this.randomData().dataPriceMonths,
+        datasets: [
+          {
+            label: "Banana",
+            fontColor: "white",
+            fill: false,
+            backgroundColor: '#fff',
+            borderColor: '#fff',
+            data: [
+              this.randomScalingFactor(),
+              this.randomScalingFactor(),
+              this.randomScalingFactor(),
+              this.randomScalingFactor(),
+              this.randomScalingFactor(),
+              this.randomScalingFactor(),
+              this.randomScalingFactor()
+            ],
+          }/*,
+          {
+          label: "Apple",
+          backgroundColor: '#45BFB3',
+          borderColor: '#45BFB3',
+          data: [
+            this._charts.randomScalingFactor(),
+            this._charts.randomScalingFactor(),
+            this._charts.randomScalingFactor(),
+            this._charts.randomScalingFactor(),
+            this._charts.randomScalingFactor(),
+            this._charts.randomScalingFactor(),
+            this._charts.randomScalingFactor()
+          ],
+          fill: false,
+        } */]
+      },
+      options: {
+        legend: {
+            label: {
+              fontColor: "white"
+            }
+        },
+        scales: { 
+          yAxes: [{
+            ticks: {
+                fontColor: "white"
+            }
+          }],
+          xAxes: [{
+            ticks: {
+              fontColor: "white"
+            }
+          }]
+        }
+      }
+    });
+  }
+ 
+  buildSpendingChart = function(element:string) {
+    return new Chart(element, {
+      type: 'pie',
+      data: {
+        labels:  this.randomData().labelSpendings ,
+        datasets: [{
+          backgroundColor: [
+            "#F8CBC5",
+            "#D9AD5B",
+            "#5CA4EB"
+          ],
+          data: this.randomData().dataSpendings
+        }] 
+      },
+      options: {
+        responsive: true,
+        legend: {
+          position: 'bottom',
+          padding: 10
+        },
+        animation: {
+          animateScale: true,
+          animateRotate: true
+        }
+      }
+    });
+  }
+
+  buildIncomeChart = function(element:string) {
+    return new Chart(element, {
+      type: 'bar',
+      data: {
+        labels: this.randomData().dataIncomeMonths,
+        datasets: [{
+          label: '$',
+          data: this.randomData().dataIncomeValues,
+          backgroundColor: '#C4C4C4'
+        }]
+      },
+      options: {
+        scales: {
+          offset: false,
+          yAxes: [{
+            gridLines: {
+              drawBorder: false,
+            },
+            ticks: {
+              beginAtZero: true
+            }
+          }],
+          xAxes: {
+            gridLines: {
+              drawBorder: false,
+              offsetGridLines: false
+            }
+          }
+        }
+      }
+    });
   }
 }
