4784be70e20ed387d3e85aa000699aee8b5be715
Izabela Lima <izamglima@gmail.com>
2020-07-31 10:55:56 +0200

    fixing e2e test

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('tostao-client app is running!');
+    expect(page.getTitleText()).toEqual('Tostão');
   });
 
   afterEach(async () => {
