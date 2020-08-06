3bb584cf267ce30e12d472a6b479c14c425ee057
Izabela Lima <izamglima@gmail.com>
2020-08-06 10:11:17 +0200

    Add sign page

---
+++ src/app/components/sign/sign.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sign',
+  templateUrl: './sign.component.html'
+})
+export class SignComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
