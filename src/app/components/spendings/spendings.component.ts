068d35db40605ce16331b23e86aefc7745fc95a3
Izabela Lima <izamglima@gmail.com>
2020-08-18 10:44:35 +0200

    spending list v0

---
+++ src/app/components/spendings/spendings.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-spendings',
+  templateUrl: './spendings.component.html'
+})
+export class SpendingsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
