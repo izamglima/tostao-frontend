3ad060f92ff83324813bc5a1dc73d62e40ebf43e
Izabela Lima <izamglima@gmail.com>
2020-08-30 12:32:44 +0200

    add dynamic new items to purchase

--- src/app/components/purchases/purchase-new/purchase-new.component.ts
+++ src/app/components/purchases/purchase-new/purchase-new.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-purchase-new',
   templateUrl: './purchase-new.component.html'
 })
 export class PurchaseNewComponent implements OnInit {
-
-  constructor() { }
+  public newPurchase: FormGroup;
+  public purchaseItem: FormArray;
+  constructor(private fb: FormBuilder) { }
 
   noteToggle = false;
   marked = false;
@@ -15,8 +16,51 @@ export class PurchaseNewComponent implements OnInit {
     this.marked = e.target.checked;
   }
 
+  addNewItem(): FormGroup {
+    return this.fb.group({
+      product: [null, Validators.compose([Validators.required])],
+      quantity: [null, Validators.compose([Validators.required])],
+      price: [null, Validators.compose([Validators.required])],
+      category: [null, Validators.compose([Validators.required])]
+    });
+  }
+
+  // add a purchaseItem form group
+  showNewItem() {
+    this.purchaseItem.push(this.addNewItem());
+  }
+
+  // remove purchaseItem from group
+  removeNewItem(index) {
+    this.purchaseItem.removeAt(index);
+  }
+
+  get purchaseItemFormGroup() {
+    return this.newPurchase.get('items') as FormArray;
+  }
+
+  cleanPurchaseNote() {
+    this.newPurchase.patchValue({'note':null});
+  }
+
+  ngOnInit(): void {
+    this.newPurchase = this.fb.group({
+      marketplace: [null, Validators.compose([Validators.required])],
+      currency: [null, Validators.compose([Validators.required])],
+      date: [null, Validators.compose([Validators.required])],
+      note: [null],
+      items: this.fb.array([this.addNewItem()])
+    });
+    // set purchaseItem to the form control containing items
+    this.purchaseItem = this.newPurchase.get('items') as FormArray;
+  }
+
+  onSubmit() {
+    console.log(this.newPurchase.value);
+  }
+
   //form object
-  newPurchase = new FormGroup({
+  /*newPurchase = new FormGroup({
     marketplace: new FormControl('', Validators.required),
     currency: new FormControl('', Validators.required),
     date: new FormControl('', Validators.required),
@@ -27,14 +71,8 @@ export class PurchaseNewComponent implements OnInit {
       price: new FormControl('', Validators.required),
       category: new FormControl('')
     })
-  })
+  })*/
 
 
-  ngOnInit(): void {
-  }
-
-  onSubmit() {
-    console.log(this.newPurchase.value);
-  }
 
 }
