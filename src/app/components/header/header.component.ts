d9f65df3529abdb9b7db2c7504d847e996f75bed
Izabela Lima <izamglima@gmail.com>
2020-09-18 12:22:12 +0200

    header logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   constructor() { }
+  logoutBoxToggle = true;
   menuContainerToggle = false;
   ngOnInit(): void {
   }
