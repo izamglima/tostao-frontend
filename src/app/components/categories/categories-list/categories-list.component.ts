93f1be0e41704f13f494762e739fbc207d8bca86
Izabela Lima <izamglima@gmail.com>
2020-10-05 10:29:42 +0200

    added filter for categories

--- src/app/components/categories/categories-list/categories-list.component.ts
+++ src/app/components/categories/categories-list/categories-list.component.ts
@@ -7,6 +7,8 @@ import { ApiService } from '../../../services/api/api.service';
 })
 export class CategoriesListComponent implements OnInit {
   categories = [];
+  filteredItems = [];
+  filterCategory;
 
   constructor(private apiService: ApiService) { }
 
@@ -14,10 +16,30 @@ export class CategoriesListComponent implements OnInit {
     this.showCategories();
   }
 
+  assignCategoriesCopy() {
+    this.filteredItems = Object.assign([], this.categories);
+  }
+
   showCategories(): void {
     this.apiService.getCategories().subscribe((response) => {
       console.log(response.data);
       this.categories = response.data;
+      this.assignCategoriesCopy();
     });
   }
+
+  filterItem(value) {
+    if(!value) {
+        this.assignCategoriesCopy();
+    }
+    this.filteredItems = Object.assign([], this.categories).filter(
+       item => item.attributes.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+    )
+  }
+
+  cleanFilter() {
+    this.filterCategory = null;
+    this.assignCategoriesCopy();
+  }
+
 }
