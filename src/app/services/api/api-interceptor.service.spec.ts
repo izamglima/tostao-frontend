444488755afbda1b679f93d07c53df29641a431e
Izabela Lima <izamglima@gmail.com>
2020-10-25 18:21:36 +0100

    fixing test for handle error on interceptor

--- src/app/services/api/api-interceptor.service.spec.ts
+++ src/app/services/api/api-interceptor.service.spec.ts
@@ -1,10 +1,11 @@
-import { TestBed } from '@angular/core/testing';
+import { getTestBed, TestBed } from '@angular/core/testing';
 import { ApiInterceptorService } from './api-interceptor.service';
 import { ApiService } from './api.service';
 import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
 import { observable, Observable } from 'rxjs';
 import { throwError } from 'rxjs/internal/observable/throwError';
+import { of } from 'rxjs/internal/observable/of';
 
 describe('ApiInterceptorService', () => {
   let service: ApiService;
@@ -43,34 +44,17 @@ describe('ApiInterceptorService', () => {
     expect(httpRequest.request.headers.has('Authorization')).toEqual(true);
   });
 
-  xit('should handle error when the request fails', () => {
-    const httpRequest = httpMock.expectOne(`${service.API_ENDPOINT}/categories`);
+  it('should handle error when the request fails - backend', () => {
 
-    httpRequest.error(<any>{}, { status: 401, statusText: "Unauthorized"});
-
-    spyOn(interceptor, 'handleError').and.returnValue(null);
-
-    expect(interceptor.handleError).toHaveBeenCalledTimes(1);
-
-    // service.getCategories().subscribe(
-    //   res => fail('should have failed with the 401 error'),
-    //   (error: HttpErrorResponse) => {
-    //     expect(error).toEqual(error, 'message');
-    //   }
-    // );
-
-  });
-
-  xit('should handle error when the request fails', () => {
     let error = new HttpErrorResponse({
       error: 401,
       statusText: 'Unauthorized'
     });
 
-    spyOn(interceptor, 'handleError').and.returnValue(throwError(error));
-
     interceptor.handleError(error);
+    spyOn(interceptor, 'handleError').and.throwError(error);
 
-    expect(interceptor.handleError).toHaveBeenCalled();
+    expect(interceptor.handleError).toThrowError()
   });
+
 });
